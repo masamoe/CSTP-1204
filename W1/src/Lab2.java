f0b7753b33d0cea5865e86c1b37df9180b2fc351
masamoe <105935014+masamoe@users.noreply.github.com>
2022-09-15 13:00:45 -0700

    lab 2

--- W1/src/Lab2.java
+++ W1/src/Lab2.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
 public class Lab2 {
-    int[] digits = new int[100];
-    void plusOne(int arr[]) {
-
+    String plusOne(int[] arr) {
+        int lastIndex = arr.length-1;
+        arr[lastIndex]++;
+        return Arrays.toString(arr);
     }
-};
+    int removeElement(int[] numbers, int val) {
+        int size = numbers.length -1;
+        int k = 0;
+
+        for(int i = 0; i < size; i++)
+            if(numbers[i] == val) {
+                for (int j = i; j < size; j++)
+                    numbers[j] = numbers[j + 1];
+                size--;
+                k++;
+            }
+
+        return k;
+    }
+}
