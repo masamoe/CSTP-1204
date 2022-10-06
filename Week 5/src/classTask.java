9e71f3a4db85b50d93869146bb828a650d96baea
masamoe <105935014+masamoe@users.noreply.github.com>
2022-10-06 09:19:09 -0700

    Create classTask.java

---
+++ Week 5/src/classTask.java
@@ -0,0 +1,23 @@
+public class classTask {
+    public static int dayOfYear(int month, int dayOfMonth, int year) {
+        int[] monthLengths = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int day = dayOfMonth;
+
+        if (month >= 2) {
+            for (int m = 1; m < month; ++m) {
+                day += monthLengths[m];
+            }
+        }
+        System.out.println(day);
+        return day;
+    }
+
+    public static void main(String[] args) {
+        int myMonth = 3;
+        int myDay = 15;
+
+        dayOfYear(myMonth, myDay, 2022);
+    }
+}
+
+
