99aa4b9f7870fa268c912f7ee481911429158971
masamoe <105935014+masamoe@users.noreply.github.com>
2022-10-06 11:07:56 -0700

    Class Task Leap Year

--- Week 5/src/classTask.java
+++ Week 5/src/classTask.java
@@ -1,9 +1,29 @@
 public class classTask {
     public enum Month {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER};
+    public static boolean isLeapYear(int year){
+        boolean isLeap = false;
+        if(year % 4 != 0){
+            isLeap = false;
+        }
+        else if(year % 100 != 0){
+            isLeap = true;
+        }
+        else if(year % 400 != 0){
+            isLeap = false;
+        }
+        else{
+            isLeap = true;
+        }
+        return isLeap;
+    }
     public static int dayOfYear(Month month, int dayOfMonth, int year) {
 
         int[] monthLengths = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int day = dayOfMonth;
+
+        if(isLeapYear(year)){
+            monthLengths[month.ordinal()+1] = 29;
+        }
 
         if (month.ordinal()+1 >= 2) {
             for (int m = 1; m < month.ordinal()+1; ++m) {
