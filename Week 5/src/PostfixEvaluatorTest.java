27ec84804d5d7b7852e475c600e8009e16bb5b1c
masamoe <105935014+masamoe@users.noreply.github.com>
2022-10-12 14:36:03 -0700

    setting up initial files

---
+++ Week 5/src/PostfixEvaluatorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PostfixEvaluatorTest {
+
+    // Implement a sufficient number of tests to have
+    // reasonable confidence in the correctness of your
+    // PostfixEvaluator implementation.
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
