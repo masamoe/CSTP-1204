5a4eaa7627ee487b5fd194b5405d26644e4c65c7
masamoe <105935014+masamoe@users.noreply.github.com>
2022-10-12 15:04:32 -0700

    Update Lab5.java

--- Week 5/src/Lab5.java
+++ Week 5/src/Lab5.java
@@ -1,6 +1,16 @@
 import java.util.Stack;
 
+
 public class Lab5 {
+    static boolean contains(char c, char[] array) {
+        for (char x : array) {
+            if (x == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         String input = "()[]{}";
         Stack<Character> myStack = new Stack<>();
@@ -8,10 +18,10 @@ public class Lab5 {
         for (char x: input.toCharArray()) {
             char[] starters = new char[]{'(', '[', '{'};
             char[] enders = new char[]{')', ']', '}'};
-            if (starters.contains(x)){
+            if (contains(x, starters)){
                 myStack.push(x);
             }
-            if (enders.contains(x)){
+            if (contains(x, enders)){
                 char comparator = myStack.pop();
                 if (x == ')'){
                     if (comparator == '('){
