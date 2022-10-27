fd07f6a77ea385c42e2d382b28dcc0f6bf32d43e
masamoe <105935014+masamoe@users.noreply.github.com>
2022-10-27 10:53:04 -0700

    Update HtmlValidator.java

--- Midterm/src/HtmlValidator.java
+++ Midterm/src/HtmlValidator.java
@@ -1,7 +1,56 @@
+import java.util.*;
+
 /*
  * This is the HtmlValidator class.
  * You should implement this class.
  */
 public class HtmlValidator {
+    private Stack<HtmlTag> stack;
+    private Queue<HtmlTag> queue;
 
+    public HtmlValidator() {
+        queue = new LinkedList<HtmlTag>();
+    }
+
+    public HtmlValidator(Queue<HtmlTag> tags) {
+        queue = new LinkedList<HtmlTag>();
+    }
+
+    public void addTag(HtmlTag tag) {
+        queue.add(tag);
+    }
+
+    public Queue<HtmlTag> getTags() {
+        return queue;
+    }
+
+    public void removeAll(String element) {
+        for (HtmlTag tag : queue) {
+            if (tag.getElement().equals(element)) {
+                queue.remove(tag);
+            }
+        }
+    }
+
+    public void validate() {
+        stack = new Stack<HtmlTag>();
+        for (HtmlTag tag : queue) {
+            if (tag.isOpenTag()) {
+                stack.push(tag);
+            } else {
+                if (stack.isEmpty()) {
+                    System.out.println("Error: " + tag.getElement() + " tag is not opened");
+                } else {
+                    HtmlTag top = stack.pop();
+                    if (!top.getElement().equals(tag.getElement())) {
+                        System.out.println("Error: " + tag.getElement() + " tag is not opened");
+                    }
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            HtmlTag top = stack.pop();
+            System.out.println("Error: " + top.getElement() + " tag is not closed");
+        }
+    }
 }
